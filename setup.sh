4a0b49aec823859698fa1c0cf6f46796602ececb
orellanaluke <67909347+orellanaluke@users.noreply.github.com>
2021-02-03 14:30:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /cloudacademy
 git clone https://github.com/cloudacademy/ca-hands-on-terratest-azure
-cp -r ./ca-hands-on-terratest-azure/* /cloudacademy/lab
+cp -r ./ca-hands-on-terratest-azure/testlab/* /cloudacademy/lab
 chmod -R 777 /cloudacademy
 
 # Replace Resource Group Name
-find "/cloudacademy" -name "*.tf" | xargs sed -i "s/REPLACEME/$2/g"
+find "/cloudacademy" -name "*.tf" | xargs sed -i "s/REPLACEME/$1/g"
