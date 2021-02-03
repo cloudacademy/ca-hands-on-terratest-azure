d365f085bd5e87061e6207723657681957f08309
orellanaluke <67909347+orellanaluke@users.noreply.github.com>
2021-02-03 14:48:12 -0500

    Update main.tf

--- testlab/examples/webserver/main.tf
+++ testlab/examples/webserver/main.tf
@@ -15,21 +15,21 @@ provider "azurerm" {
 
 #Get CloudAcademy Lab Resource Group
 data "azurerm_resource_group" "rg" {
-  name = "REPLACEME"
+  name = "cal-956-2"
 }
 
 #Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-modchallenge-${azurerm_resource_group.rg.location}-001"
+  name                = "vnet-modchallenge-${data.azurerm_resource_group.rg.location}-001"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-modchallenge-${azurerm_resource_group.rg.location}-001"
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = "snet-modchallenge-${data.azurerm_resource_group.rg.location}-001"
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
@@ -38,10 +38,10 @@ module "webserver" {
     source = "../../"
 
     subnet_id = azurerm_subnet.subnet.id
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
+    resource_group_name = data.azurerm_resource_group.rg.name
+    location = data.azurerm_resource_group.rg.location
 
     servername = var.servername
-    size = "Basic_A1"
+    vm_size = "Basic_A1"
 
 }
